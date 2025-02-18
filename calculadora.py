daf8f572ef996d845beec70ea7ff9bf52786bf05
Jhon Castro <jhon.castro07@uptc.edu.co>
2025-02-18 10:16:01 -0500

    Agregar Operacion Multiplicacion

--- calculadora.py
+++ calculadora.py
@@ -2,13 +2,16 @@ def suma(a, b):
     return a + b
 def resta(a, b):
     return a - b
+def multiplicacion(a, b):
+    return a * b
 
 def calculadora():
     print("Calculadora Básica")
     print("1. Suma")
     print("2. Resta")
+    print("3. Multiplicación")
     
-    opcion = input("Elige una opción (1/2): ")
+    opcion = input("Elige una opción (1/2/3): ")
     
     if opcion in ('1', '2'):
         try:
@@ -17,8 +20,10 @@ def calculadora():
             
             if opcion == '1':
                 print("Resultado:", suma(num1, num2))
-            if opcion == '2':
+            elif opcion == '2':
                 print("Resultado:", resta(num1, num2))
+            elif opcion == '3':
+                print("Resultado:", multiplicacion(num1, num2))
         except ValueError:
             print("Error: Ingresa valores numéricos válidos")
     else:
